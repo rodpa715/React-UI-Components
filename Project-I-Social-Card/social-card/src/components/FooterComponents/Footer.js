61cf41d050c92b1aaa59a4ec37625c3fa4ce185c
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-27 16:31:01 +0200

    tidy up code

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -2,18 +2,24 @@ import React from "react";
 import "./Footer.css";
 
 function FooterContainer() {
-    return(
-        <div className="footer-container">
-            <div className="icons comments" ></div>
-            <div className="retweet">
-            <div className="icons" id="retweet"></div><span></span>
-            </div>
-            <div className="like" >
-            <div className="icons" id="like"></div><span></span>
-            </div>
-            <div className="icons send-dm"></div>
-        </div>
-    );
+  let count = 0;
+  function addOne() {
+    count === 0 ? count++ : (count = 0);
+  }
+  return (
+    <div className="footer-container">
+      <div className="icons comments" />
+      <div className="retweet">
+        <div className="icons" id="retweet" onClick={addOne} />
+        <span>{state.count}</span>
+      </div>
+      <div className="like">
+        <div className="icons" id="like" onClick={addOne} />
+        <span>{state.count}</span>
+      </div>
+      <div className="icons send-dm" />
+    </div>
+  );
 }
 
 export default FooterContainer;
