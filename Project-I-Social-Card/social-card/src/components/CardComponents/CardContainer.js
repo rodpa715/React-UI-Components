88ff806164f0dfb5122f182868456ab2fb580ac3
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-27 15:41:57 +0200

    import components & exports

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,19 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
+
+function CardContainer({ imageSource, contents }) {
+  return (
+    <div className="card-container">
+      <CardBanner alt={imageSource.alt} src={imageSource.src} />
+      <CardContent
+        heading={contents.heading}
+        text={contents.text}
+        source={contents.source}
+      />
+    </div>
+  );
+}
+
+export default CardContainer;
