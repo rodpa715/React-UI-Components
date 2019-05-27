868fa3a54c1377bd99b80cfb40388f43996cd1d3
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-27 16:18:41 +0200

    mvp complete

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -6,10 +6,10 @@ function FooterContainer() {
         <div className="footer-container">
             <div className="icons comments" ></div>
             <div className="retweet">
-            <div className="icons" id="retweet"></div><span>1</span>
+            <div className="icons" id="retweet"></div><span></span>
             </div>
             <div className="like" >
-            <div className="icons" id="like"></div><span>1</span>
+            <div className="icons" id="like"></div><span></span>
             </div>
             <div className="icons send-dm"></div>
         </div>
