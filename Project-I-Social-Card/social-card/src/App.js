712ea415372828f1ba570b887e5d79538eda916c
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-27 16:15:07 +0200

    import footer

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
 import CardContainer from "./components/CardComponents/CardContainer";
+import FooterContainer from "./components/FooterComponents/Footer"
 var moment = require('moment');
 
 const imageSource = {
@@ -41,6 +42,7 @@ const App = () => {
         headerText={headerText}
       />
       <CardContainer imageSource={cardImage} contents={cardContents}/>
+      <FooterContainer />
     </div>
   );
 };
