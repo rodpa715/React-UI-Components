4aa6864698bfc43bf578a671bc532f79a2b16aea
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-28 13:42:58 +0200

    flesh out component structure

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Button.css';
+
+function OperatorsComponent(){
+  return(
+    <div className="operators">
+      <div className="divide">÷</div>
+      <div className="multiply">X</div>
+      <div className="subtract">-</div>
+      <div className="add">+</div>
+      <div className="execute">=</div>
+    </div>
+  )
+}
+
+export default OperatorsComponent;
