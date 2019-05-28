8a4e07d6d85330a287308e8c65e153797004762e
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-28 16:31:02 +0200

    add keys to operators

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -5,7 +5,7 @@ import "./Button.css";
 function OperatorsComponent( {operatorsArray} ) {
   return (
     <div className="operators">
-      {operatorsArray.map(operator => <div className={operator.name}>{operator.char}</div>)}
+      {operatorsArray.map(operator => <div key={operator.name} className={operator.name}>{operator.char}</div>)}
     </div>
   );
 }
