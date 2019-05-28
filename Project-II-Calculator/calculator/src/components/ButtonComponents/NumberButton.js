d56435f14b94f861d55326b40f741234927ddb79
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-28 13:43:14 +0200

    flesh out component structure

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Button.css';
+
+function KeypadComponent() {
+  return(
+    <div className="Keypad">
+      <div className="clear">Clear</div>
+      <div className="numbers">
+        <div data-id="1">1</div>
+        <div data-id="2">2</div>
+        <div data-id="3">3</div>
+        <div data-id="4">4</div>
+        <div data-id="5">5</div>
+        <div data-id="6">6</div>
+        <div data-id="7">7</div>
+        <div data-id="8">8</div>
+        <div data-id="9">9</div>
+        <div data-id="0">0</div>
+      </div>
+    </div>
+  )
+}
+
+export default KeypadComponent;
