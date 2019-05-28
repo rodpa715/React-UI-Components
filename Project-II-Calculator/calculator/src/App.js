7b5a0c1f470d89f8663ea2ccaa807623e6107caa
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-28 15:54:47 +0200

    MVP COMPLETE

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import OperatorsComponent from './components/ButtonComponents/ActionButton';
 import KeypadComponent from './components/ButtonComponents/KeypadComponent';
