dc5b5ff54243ed914bba5f49215c7e3da360929a
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-28 13:43:41 +0200

    fleshout component structure

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+function displayComponent() {
+  return(
+    <div className="display">
+      <h1 className="numbers">0</h1>
+    </div>
+  )
+}
+
+export default displayComponent;
